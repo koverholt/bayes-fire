8a80ad57680c78b3c475ae491f2714dbe30a550d
mobruns@gmail.com <mobruns@gmail.com@41f8ace8-4f22-f401-62e0-1da6b7b4aaa8>
2013-09-27 21:22:52 +0000

    Gypsum Model: Started working on external_gyp1d.py

--- Example_Cases/Gypsum_Model/Scripts/external_gyp1d.py
+++ Example_Cases/Gypsum_Model/Scripts/external_gyp1d.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Module for FDS functions"""
+"""Module for gyp1d functions"""
 
 import numpy as np
 import platform
@@ -11,120 +11,88 @@ import os
 op_sys = platform.system()
 
 
-def gen_input(abs_coeff, A, E, emissivity, HoR, k, rho, c):
-    """Generate FDS input file from template.
+def gen_input( matl ):
+    """Generate gyp1d input file from template.
 
     Keyword arguments:
-    abs_coeff -- Absorption coefficient of solid material
-    A -- Pre exponential of solid material
-    E -- Activation energy of solid material
-    emissivity -- Emissivity of solid material
-    HoR -- Heat of reaction of solid material
-    k -- Thermal conductivity of solid material
-    rho -- Density of solid material
-    c -- Specific heat of solid material
+    matl = [ k1, k2, k3, k4, rho_0, c_p1, c_p2, c_p3, eps, Y1_0,
+             A1, A2, E1, E2, dh1, dh2 ]
     """
 
-    template = """&HEAD CHID='case', TITLE='Black PMMA at 50 kW/m2, No Gas Phase Reaction' /
+    template = """
+&matl 
+    k_temps = 273.15, 448.15, 1088.15, 1473.15
+    k_vals = %(k1)s, %(k2)s, %(k3)s, %(k4)s
+    rho_0 = %(rho_0)s
+    c_p = %(c_p1)s, %(c_p2)s, %(c_p3)s
+    eps = %(eps)s 
+    Y1_0 = %(Y1_0)s
+    A = %(A1)s, %(A2)s
+    E = %(E1)s, %(E2)s
+    dh = %(dh1)s, %(dh2)s /
 
-&MESH IJK=3,3,4, XB=-0.15,0.15,-0.15,0.15,0.0,0.4 /  Mesh is just to make FDS run
+&scen
+    L = 0.0159
+    L_a = 0.092
+    t_end = 3601
+    H = 3.048 /
 
-&TIME T_END=515., WALL_INCREMENT=1, DT=0.1 /  Force FDS to do solid phase calc every time step
-&MISC  Y_O2_INFTY=0.01 /
-
-&REAC FUEL='METHANE'/  No gas phase reaction with 1%% O2 concentration
-
-&MATL ID='BLACKPMMA'
-      ABSORPTION_COEFFICIENT=%(abs_coeff)s
-      N_REACTIONS=1
-      A(1) = %(A)s
-      E(1) = %(E)s
-      EMISSIVITY=%(emissivity)s
-      SPEC_ID='METHANE'
-      NU_SPEC=1.
-      HEAT_OF_REACTION=%(HoR)s
-      HEAT_OF_COMBUSTION=25200.
-      CONDUCTIVITY=%(k)s
-      DENSITY=%(rho)s
-      SPECIFIC_HEAT=%(c)s /
-
-&MATL ID='FOAMGLAS'
-      CONDUCTIVITY = 0.08
-      SPECIFIC_HEAT = 0.84
-      DENSITY = 120. /
-
-&SURF ID='PMMA SLAB'
-      COLOR='BLACK'
-      MATL_ID='BLACKPMMA','FOAMGLAS'
-      THICKNESS=0.0085,0.01
-      HEAT_TRANSFER_COEFFICIENT=0.
-      EXTERNAL_FLUX=52 /  External Flux is ONLY for this simple demo exercise
-
-&VENT XB=-0.05,0.05,-0.05,0.05,0.0,0.0, SURF_ID = 'PMMA SLAB' /
-
-&VENT MB='XMIN', SURF_ID='OPEN' /
-&VENT MB='XMAX', SURF_ID='OPEN' /
-&VENT MB='YMIN', SURF_ID='OPEN' /
-&VENT MB='YMAX', SURF_ID='OPEN' /
-&VENT MB='ZMAX', SURF_ID='OPEN' /
-
-&DUMP DT_DEVC=5. /
-
-&DEVC XYZ=0.0,0.0,0.0, IOR=3, QUANTITY='WALL TEMPERATURE',     ID='temp' /
-&DEVC XYZ=0.0,0.0,0.0, IOR=3, QUANTITY='BURNING RATE',         ID='MLR' /
-&DEVC XYZ=0.0,0.0,0.0, IOR=3, QUANTITY='WALL THICKNESS',       ID='thick' /
-
-&TAIL /"""
+&numr
+    N = 30
+    N_t = 160000
+    N_sol = 100 /
+"""
+    print template
 
     #  ==================================================
-    #  = Generate FDS input file and fire object file =
+    #  = Generate gyp1d input file                      =
     #  ==================================================
 
-    outcase = template % {'abs_coeff':str(abs_coeff),
-                          'A':str(A),
-                          'E':str(E),
-                          'emissivity':str(emissivity),
-                          'HoR':str(HoR),
-                          'k':str(k),
-                          'rho':str(rho),
-                          'c':str(c)}
-
-    #  =====================
-    #  = Write FDS files =
-    #  =====================
-
-    casename = 'case'
-    filename = '../../../FDS_Model/' + casename + '.fds'
-
-    # Opens a new file, writes the FDS input file, and closes the file
-    f = open(filename, 'w')
-    f.write(outcase)
-    f.close()
-
-    return casename
-
-
-def run_fds(casename):
-    """Run FDS on case file."""
-    os.chdir('../../../FDS_Model')
-
-    # Run appropriate executable depending on operating system
-    if op_sys == 'Linux':
-        p = subprocess.Popen(['./fds_intel_linux_64', casename + '.fds'])
-        p.wait()
-    if op_sys == 'Darwin':
-        p = subprocess.Popen(['./fds_intel_osx_64', casename + '.fds'])
-        p.wait()
-    if op_sys == 'Windows':
-        p = subprocess.Popen(['./fds_intel_win_64', casename + '.fds'])
-        p.wait()
-
-    os.chdir('../Example_Cases/FDS_Mass_Loss_Rate/Scripts')
-
-
-def read_fds(casename):
-    """Read in FDS output."""
-    mlr_file = '../../../FDS_Model/' + casename + '_devc.csv'
-    mlrs = np.genfromtxt(mlr_file, delimiter=',', skip_header=2)
-
-    return mlrs
+#    outcase = template % {'abs_coeff':str(abs_coeff),
+#                          'A':str(A),
+#                          'E':str(E),
+#                          'emissivity':str(emissivity),
+#                          'HoR':str(HoR),
+#                          'k':str(k),
+#                          'rho':str(rho),
+#                          'c':str(c)}
+#
+#    #  =====================
+#    #  = Write FDS files =
+#    #  =====================
+#
+#    casename = 'case'
+#    filename = '../../../FDS_Model/' + casename + '.fds'
+#
+#    # Opens a new file, writes the FDS input file, and closes the file
+#    f = open(filename, 'w')
+#    f.write(outcase)
+#    f.close()
+#
+#    return casename
+#
+#
+#def run_fds(casename):
+#    """Run FDS on case file."""
+#    os.chdir('../../../FDS_Model')
+#
+#    # Run appropriate executable depending on operating system
+#    if op_sys == 'Linux':
+#        p = subprocess.Popen(['./fds_intel_linux_64', casename + '.fds'])
+#        p.wait()
+#    if op_sys == 'Darwin':
+#        p = subprocess.Popen(['./fds_intel_osx_64', casename + '.fds'])
+#        p.wait()
+#    if op_sys == 'Windows':
+#        p = subprocess.Popen(['./fds_intel_win_64', casename + '.fds'])
+#        p.wait()
+#
+#    os.chdir('../Example_Cases/FDS_Mass_Loss_Rate/Scripts')
+#
+#
+#def read_fds(casename):
+#    """Read in FDS output."""
+#    mlr_file = '../../../FDS_Model/' + casename + '_devc.csv'
+#    mlrs = np.genfromtxt(mlr_file, delimiter=',', skip_header=2)
+#
+#    return mlrs
